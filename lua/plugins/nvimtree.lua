6df0e37b0afb097db235d41c77d5c65d7420366c
Thuấn Đỗ Hoàng <thuan.dh@teko.vn>
2022-03-08 19:13:13 +0700

    setting nvimtree keymap

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -4,18 +4,18 @@ if not present then
 end
 
 tree.setup {
-  disable_netrw        = false,
-  hijack_netrw         = true,
-  open_on_setup        = false,
+  disable_netrw = false,
+  hijack_netrw = true,
+  open_on_setup = false,
   ignore_buffer_on_setup = false,
-  ignore_ft_on_setup   = {},
-  auto_close           = false,
+  ignore_ft_on_setup = {},
+  auto_close = false,
   auto_reload_on_write = true,
-  open_on_tab          = false,
-  hijack_cursor        = false,
-  update_cwd           = false,
+  open_on_tab = false,
+  hijack_cursor = false,
+  update_cwd = false,
   hijack_unnamed_buffer_when_opening = false,
-  hijack_directories   = {
+  hijack_directories = {
     enable = true,
     auto_open = true,
   },
@@ -26,20 +26,20 @@ tree.setup {
       info = "",
       warning = "",
       error = "",
-    }
+    },
   },
   update_focused_file = {
-    enable      = true,
-    update_cwd  = false,
-    ignore_list = {}
+    enable = true,
+    update_cwd = false,
+    ignore_list = {},
   },
   system_open = {
-    cmd  = nil,
-    args = {}
+    cmd = nil,
+    args = {},
   },
   filters = {
     dotfiles = false,
-    custom = {}
+    custom = {},
   },
   git = {
     enable = true,
@@ -50,23 +50,58 @@ tree.setup {
     width = 50,
     height = 30,
     hide_root_folder = false,
-    side = 'right',
+    side = "right",
     preserve_window_proportions = false,
     mappings = {
-      custom_only = false,
+      custom_only = true,
       list = {
-        { key = "v", action = "vsplit" },
-        { key = "s", action = "split" },
+        { key = { "<CR>", "o", "l" }, action = "edit" },
+        { key = "<C-o>", action = "edit_in_place" },
+        { key = { "O" }, action = "edit_no_picker" },
+        { key = { "<2-RightMouse>", "<C-]>" }, action = "cd" },
+        { key = "<C-v>", action = "vsplit" },
+        { key = "<C-x>", action = "split" },
+        { key = "<C-t>", action = "tabnew" },
+        { key = "<", action = "prev_sibling" },
+        { key = ">", action = "next_sibling" },
+        { key = "P", action = "parent_node" },
+        { key = "h", action = "close_node" },
+        { key = "<Tab>", action = "preview" },
+        { key = "K", action = "first_sibling" },
+        { key = "J", action = "last_sibling" },
+        { key = "I", action = "toggle_ignored" },
+        { key = "H", action = "toggle_dotfiles" },
+        { key = "R", action = "refresh" },
+        { key = "a", action = "create" },
+        { key = "d", action = "remove" },
+        { key = "D", action = "trash" },
+        { key = "r", action = "rename" },
+        { key = "<C-r>", action = "full_rename" },
+        { key = "x", action = "cut" },
+        { key = "c", action = "copy" },
+        { key = "p", action = "paste" },
+        { key = "y", action = "copy_name" },
+        { key = "Y", action = "copy_path" },
+        { key = "gy", action = "copy_absolute_path" },
+        { key = "[c", action = "prev_git_item" },
+        { key = "]c", action = "next_git_item" },
+        { key = "-", action = "dir_up" },
+        { key = "s", action = "system_open" },
+        { key = "q", action = "close" },
         { key = "?", action = "toggle_help" },
-      }
+        { key = "W", action = "collapse_all" },
+        { key = "S", action = "search_node" },
+        { key = "<C-k>", action = "toggle_file_info" },
+        { key = ".", action = "run_file_command" },
+      },
     },
     number = false,
     relativenumber = false,
-    signcolumn = "yes"
+    signcolumn = "yes",
   },
   trash = {
     cmd = "trash",
-    require_confirm = true
+    require_confirm = true,
   },
   actions = {
     change_dir = {
@@ -80,11 +115,10 @@ tree.setup {
         enable = true,
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
         exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame", },
-          buftype  = { "nofile", "terminal", "help", },
-        }
-      }
-    }
-  }
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
+  },
 }
-
